cd87cfb9eb171d8576ddc0f09d352e543dfc0f0a
Spencer <spencerpenrod@gmail.com>
2020-08-21 09:03:48 -0600

    Add serialize method to Post class

--- project4/network/models.py
+++ project4/network/models.py
@@ -12,3 +12,12 @@ class Post(models.Model):
     text = models.TextField(max_length=280)
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="users")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+            "user": self.user,
+            "text": self.text,
+            "likes": self.likes
+        }
